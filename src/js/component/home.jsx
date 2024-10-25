ab65975420a214df62287686ad5c53266e7fa5c0
José Antonio Llorens Padilla <131461633+JoseAnt7@users.noreply.github.com>
2024-10-25 16:33:08 +0000

    estructura_semáforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
+	const [ color, setColor] = useState("red");
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container d-flex flex-column align-items-center justify-content-center">
+			<div className="trafficTop bg-dark"></div>
+			<div className="d-flex flex-column align-items-center justify-content-center base">
+				<div className="red light rounded-circle bg-danger my-2"></div>
+				<div className="yellow light rounded-circle bg-warning my-2"></div>
+				<div className="green light rounded-circle bg-success my-2"></div>
+			</div>
 		</div>
 	);
 };
