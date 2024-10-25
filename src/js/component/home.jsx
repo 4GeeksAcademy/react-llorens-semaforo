61ed9462201efe16f12e8c71e78d51c154f7d98e
José Antonio Llorens Padilla <131461633+JoseAnt7@users.noreply.github.com>
2024-10-25 18:39:37 +0000

    hecho

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,15 +6,17 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 
 const Home = () => {
-	const [ color, setColor] = useState("red");
+	const [ color, setColor] = useState(" ");
+	
 	return (
 		<div className="container d-flex flex-column align-items-center justify-content-center">
 			<div className="trafficTop bg-dark"></div>
 			<div className="d-flex flex-column align-items-center justify-content-center base">
-				<div className="red light rounded-circle bg-danger my-2"></div>
-				<div className="yellow light rounded-circle bg-warning my-2"></div>
-				<div className="green light rounded-circle bg-success my-2"></div>
+				<div onClick={()=>setColor('rojo')} className={`red light rounded-circle bg-danger my-2 ${color === 'rojo' ? 'iluminado': ''}`}></div>
+				<div onClick={()=>setColor('amarillo')}  className={`yellow light rounded-circle bg-warning my-2 ${color === 'amarillo' ? 'iluminado': ''}`}></div>
+				<div onClick={()=>setColor('verde')}  className={`green light rounded-circle bg-success my-2 ${color === 'verde' ? 'iluminado': ''}`}></div>
 			</div>
+			<button className="btn btn-success">Cambiar</button>
 		</div>
 	);
 };
